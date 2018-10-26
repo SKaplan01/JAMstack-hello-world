d20521f60694456e6368efd32d5d7f40af478316
Sarah Kaplan <skaplan520@gmail.com>
2018-10-26 13:01:15 -0700

    adding boilerplate for gatsby site

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi everyone</h1>
+    <p>I'm Sarah.</p>
+    <p>
+      I'm a student at Rithm School and want to practice my Javascript skills.
+    </p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
